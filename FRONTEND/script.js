98e88d97bf3db1293c7425ee90faf8a55999bffd
kiss.balint.hu <kiss.balint.hu@gmail.com>
2025-03-13 18:22:58 +0100

    Input mezők átalakítása, pontosítása, a dátum bekérése a felhasználótól, hónap legördülő menü

--- FRONTEND/script.js
+++ FRONTEND/script.js
@@ -17,12 +17,12 @@ class Income {
         this.quantity = 0
         this.incomeDiv = incomeDiv
 
-        const saveButton = incomeDiv.querySelector('.saveButton');
+        //const saveButton = incomeDiv.querySelector('.saveButton');
         const deleteButton = incomeDiv.querySelector('.deleteButton');
 
-        saveButton.addEventListener('click', function () {
+        /*saveButton.addEventListener('click', function () {
             console.log('Erre a gombra kattintottál:' + saveButton);
-        })
+        })*/
         deleteButton.addEventListener('click', function () {
             console.log('Erre a gombra kattintottál:' + deleteButton);
             incomesDiv.removeChild(incomeDiv)
@@ -35,13 +35,11 @@ class Income {
         this.quantity = quantity
     }
 
-    delete()
-    {
+    delete() {
         console.log("Delete")
     }
 
-    change()
-    {
+    change() {
         this.isSaved = false
     }
 
@@ -54,13 +52,30 @@ function addIncome() {
     const newIncome = document.createElement('div');
     newIncome.classList.add('input-container');
     newIncome.innerHTML = `
-        <div>Bevétel típúsa: </div>
+        <label>Év: </label>
+        <input type="number" placeholder="Adj meg egy évszámot!" min="0" step="1">
+        <label>Hónap: </label>
+        <select class="montChoosing" name="montChoosing">
+            <option value=1>január</option>
+            <option value=1>február</option>
+            <option value=3>március</option>
+            <option value=4>április</option>
+            <option value=5>május</option>
+            <option value=6>június</option>
+            <option value=7>július</option>
+            <option value=8>augusztus</option>
+            <option value=9>szeptember</option>
+            <option value=10>október</option>
+            <option value=11>november</option>
+            <option value=12>december</option>
+        </select>
+        <label>Bevétel típúsa: </label>
         <input type="text" placeholder="Mire kaptad a pénzt?">
-        <div>Bevétel nagysága: </div>
-        <input type="number" placeholder="Mennyit kaptál?">
-        <button class="saveButton">Mentés</button>
+        <label>Bevétel nagysága: </label>
+        <input type="number" placeholder="Mennyit kaptál?" min="0" step="1">
+
         <button class="deleteButton">Törlés</button>
-`;
+`
 
     incomesDiv.appendChild(newIncome);
 
